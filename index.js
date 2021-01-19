eb1804dbac946be49ac95467fced0bf72ec407c4
Margarita Morozova <ritamorozova@yahoo.com>
2021-01-19 12:51:40 -0800

    commit tree 5cbd9d8416a85455634c227d7dc4791612d73ad9

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
+function findMinAndRemoveSorted(array){
+  let min=array[0]
+  let indexOfMin= 0
 
+  for(let i=0; i<array.length; i++){
+      if(array[i] < min){
+          min=array[i]
+          indexOfMin = i 
+      }
+  }
+  array.splice(indexOfMin, 1)
+  return min
+}
+
+function merge(firstHalf, secondHalf){
+    let sorted=[]
+    while(firstHalf.length !=0 && secondHalf !=0){
+        if(firstHalf[0]<secondHalf[0]){
+            sorted.push(findMinAndRemoveSorted(firstHalf))
+        }else{
+            sorted.push(findMinAndRemoveSorted(secondHalf))
+        }
+    }
+    return sorted.concat(firstHalf).concat(secondHalf)
+}
+
+function mergeSort(array){
+    let midpoint = array.length /2 
+    let firstHalf = array.slice(0, midpoint)
+    let secondHalf = array.slice(midpoint, array.length)
+    let sorted
+    if(array.length < 2){
+        return array
+    }else{
+        return merge(mergeSort(firstHalf), mergeSort(secondHalf))
+    }
+    return sorted
+}
